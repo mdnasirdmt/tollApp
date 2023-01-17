21de3971fc4c184a61886b0abf347fa67f30d211
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-01-17 18:12:25 +0530

    input validation success

--- TollCalculator/src/tollPoint/ListOfFastagAndCollectToll.java
+++ TollCalculator/src/tollPoint/ListOfFastagAndCollectToll.java
@@ -1,28 +1,41 @@
 package tollPoint;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import vehicleDetails.CollectToll;
 import vehicleDetails.Fastag;
 
 public class ListOfFastagAndCollectToll {
-	
-	
-	public static void listOfFastagAndCollectToll(List<Fastag> fastags, List<CollectToll> collectTolls ) {
-		
-		
-		for(Fastag fastag : fastags) {
-			
-			System.out.println(fastag.toString());
+
+	public static void listOfFastagAndCollectToll(List<Fastag> fastags, List<CollectToll> collectTolls) {
+
+		for (Fastag fastag : fastags) {
+
+//			System.out.println(fastag.toString());
 		}
-		
-		for(CollectToll collectToll : collectTolls) {
-			
-			System.out.println(collectToll.toString());
+
+		for (CollectToll collectToll : collectTolls) {
+
+//			System.out.println(collectToll.toString());
 		}
-		
-		
-		
+
+//		List<String> collect = collectTolls.stream().map(c-> c.getVehicleNumber()).collect(Collectors.toList());
+//		List<String> collect2 = fastags.stream().map(c-> c.getVehicleNumber()).collect(Collectors.toList());
+//		System.out.println(collect2);
+//		System.out.println(collect);
+
+		for (CollectToll collectToll : collectTolls) {
+
+			for (Fastag fastag : fastags) {
+
+				if (collectToll.getVehicleNumber().equals(fastag.getVehicleNumber())) {
+					System.out.println(collectToll.getVehicleNumber() + " " + collectToll.getVehicleType()
+							+ " have fastag with balance " + fastag.getFastagBalance());
+				}
+			}
+		}
+
 	}
 
 }
