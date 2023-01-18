01f8fa386f9dcd56bb8661ab31f875bd3e486318
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-01-18 19:33:43 +0530

    payment summary class done

--- TollCalculator/src/printCollections/PaymentSummary.java
+++ TollCalculator/src/printCollections/PaymentSummary.java
@@ -3,9 +3,25 @@ package printCollections;
 public class PaymentSummary {
 
 	int fastagAmountCollected;
-	
+
 	int cashAmountCollected;
-		
+
+	public PaymentSummary() {
+
+	}
+
+	public PaymentSummary(int fastagAmountCollected, int cashAmountCollected) {
+		super();
+		this.fastagAmountCollected = fastagAmountCollected;
+		this.cashAmountCollected = cashAmountCollected;
+	}
+
+	@Override
+	public String toString() {
+		return "PaymentSummary [fastagAmountCollected=" + fastagAmountCollected + ", cashAmountCollected="
+				+ cashAmountCollected + "]";
+	}
+
 }
 
 //PAYMENT_SUMMARY <FASTAG_AMOUNT_COLLECTED> <CASH_AMOUNT_COLLECTED>
--- TollCalculator/src/tollPoint/ListOfFastagAndCollectToll.java
+++ TollCalculator/src/tollPoint/ListOfFastagAndCollectToll.java
@@ -4,12 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import printCollections.PaymentSummary;
 import vehicleDetails.CollectToll;
 import vehicleDetails.Fastag;
 
 public class ListOfFastagAndCollectToll {
 
 	public static void listOfFastagAndCollectToll(List<Fastag> fastags, List<CollectToll> collectTolls) {
+		
+		
 
 //		for (Fastag fastag : fastags) {
 //
@@ -42,25 +45,134 @@ public class ListOfFastagAndCollectToll {
 //		}
 
 		///////////////////////
-		
-		List<String> collect2 = fastags.stream().map(c -> c.getVehicleNumber()).collect(Collectors.toList());
-		
-		int sumFastag=0, sumCash=0;
-		
-		for (CollectToll collectToll : collectTolls) {
 
-			if (collect2.contains(collectToll.getVehicleNumber())) {
-//			System.out.println(collectToll);
-				
-				if(collectToll.getVehicleType().equals("CAR")) {
-					
+
+		int sumFastag = 0, sumCash = 0;
+
+		for (CollectToll collectToll : collectTolls) {
+			for (Fastag fast : fastags) {
+
+				if (collectToll.getVehicleNumber().equals(fast.getVehicleNumber())) {
+
+					/// bus
+					if (collectToll.getVehicleType().equals("BUS")) {
+
+						if (fast.getFastagBalance() >= VehicleCharge.tollChargeBus) {
+
+							sumFastag += VehicleCharge.tollChargeBus;
+							
+						} else if (fast.getFastagBalance() < VehicleCharge.tollChargeBus) {
+							sumFastag += fast.getFastagBalance();
+							sumCash += VehicleCharge.tollChargeBus - fast.getFastagBalance() + 40;						
+						}
+					}
+
+					/// car
+					else if (collectToll.getVehicleType().equals("CAR")) {
+
+						if (fast.getFastagBalance() >= VehicleCharge.tollChargeCar) {
+
+							sumFastag += VehicleCharge.tollChargeCar;
+
+						} else if (fast.getFastagBalance() < VehicleCharge.tollChargeCar) {
+
+							sumFastag += fast.getFastagBalance();
+							sumCash += (VehicleCharge.tollChargeCar - fast.getFastagBalance()) + 40;
+						}
+					}
+
+					/// motorbike
+					else if (collectToll.getVehicleType().equals("MOTORBIKE")) {
+
+						if (fast.getFastagBalance() >= VehicleCharge.tollChargeMotorbike) {
+
+							sumFastag += VehicleCharge.tollChargeMotorbike;
+
+						} else if (fast.getFastagBalance() < VehicleCharge.tollChargeMotorbike) {
+
+							sumFastag += fast.getFastagBalance();
+							sumCash += (VehicleCharge.tollChargeMotorbike - fast.getFastagBalance()) + 40;
+						}
+					}
+
+					/// Ricksha
+					else if (collectToll.getVehicleType().equals("RICKSHAW")) {
+
+						if (fast.getFastagBalance() >= VehicleCharge.tollChargeRickshaw) {
+
+							sumFastag += VehicleCharge.tollChargeRickshaw;
+
+						} else if (fast.getFastagBalance() < VehicleCharge.tollChargeRickshaw) {
+
+							sumFastag += fast.getFastagBalance();
+							sumCash += (VehicleCharge.tollChargeRickshaw - fast.getFastagBalance()) + 40;
+						}
+					}
+
+					// scooter
+					else if (collectToll.getVehicleType().equals("SCOOTER")) {
+
+						if (fast.getFastagBalance() >= VehicleCharge.tollChargeScooter) {
+
+							sumFastag += VehicleCharge.tollChargeScooter;
+
+						} else if (fast.getFastagBalance() < VehicleCharge.tollChargeScooter) {
+
+							sumFastag += fast.getFastagBalance();
+							sumCash += (VehicleCharge.tollChargeScooter - fast.getFastagBalance()) + 40;
+						}
+					}
+
+					/// truck
+					else if (collectToll.getVehicleType().equals("TRUCK")) {
+
+						if (fast.getFastagBalance() >= VehicleCharge.tollChargeTruck) {
+
+							sumFastag += VehicleCharge.tollChargeTruck;
+
+						} else if (fast.getFastagBalance() < VehicleCharge.tollChargeTruck) {
+
+							sumFastag += fast.getFastagBalance();
+							sumCash += (VehicleCharge.tollChargeTruck - fast.getFastagBalance()) + 40;
+						}
+					}
+
+					// van
+					else if (collectToll.getVehicleType().equals("VAN")) {
+
+						if (fast.getFastagBalance() >= VehicleCharge.tollChargeVan) {
+
+							sumFastag += VehicleCharge.tollChargeTruck;
+
+						} else if (fast.getFastagBalance() < VehicleCharge.tollChargeVan) {
+
+							sumFastag += fast.getFastagBalance();
+							sumCash += (VehicleCharge.tollChargeVan - fast.getFastagBalance()) + 40;
+						}
+					}
 				}
 
-			} else {
-//			System.out.println(collectToll);
 			}
 
 		}
+
+		// if vehicle doesn't have fastag
+		List<String> listNumber = fastags.stream().map(c -> c.getVehicleNumber()).collect(Collectors.toList());
+		List<String> collect = collectTolls.stream().map(c -> c.getVehicleNumber()).collect(Collectors.toList());
+		for (String collectToll : collect) {
+			boolean f = listNumber.contains(collectToll);
+			if (f == false) {
+				sumCash += VehicleCharge.tollChargeCar + 40;
+			}
+
+		}
+
+//		System.out.println(sumFastag);
+//		System.out.println(sumCash);
+		
+		PaymentSummary paymentSummary= new PaymentSummary(sumFastag, sumCash);
+		System.out.println(paymentSummary.toString());
+		
 
 		/////////////////////////////////////
 
